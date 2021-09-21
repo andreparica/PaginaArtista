0e968a658601d325e476b89ee10d1586ae30b24b
andreparica <geniusparica@gmail.com>
2021-09-21 18:57:54 -0500

    primer commit

---
+++ Javascript/java.js
@@ -0,0 +1,36 @@
+/*alert ("Esto es The Mills: ¡Bienvenido!");
+console.log ("Hola Usuario!");
+prompt ("Ingresa tu nombre");
+confirm ("¿Quiéres continuar en esta página?");*/
+
+const album1 = () => {
+    alert ("Escucha a Babel");
+    
+    }
+
+const album2 = () => {
+    alert ("Escucha Babel Special Edition");
+
+
+    }
+
+const album3 = () => {
+    alert ("Escucha Señora");
+
+
+    }
+
+const album4 = () => {
+    alert (" Escucha Volver a Empezar");
+
+    }
+
+const album5 = () => {
+    alert (" Esucha 10 Años");
+    
+    }
+
+const album6 = () => {
+alert (" Escucha nOlvidarte");
+        
+    }
